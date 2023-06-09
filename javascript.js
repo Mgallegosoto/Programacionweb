ea63afcab2bac035550c2a877e20ada30979cb7a
Mgallegosoto <mgallegosoto@itba.edu.ar>
2023-06-09 00:27:37 -0300

    push

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// var edad = prompt("Por favor, ingresa tu edad:");
+
+// if (isNaN(edad)) {
+//   alert("Ingresaste un valor inválido. Por favor, ingresa tu edad nuevamente.");
+// } else {
+//   if (edad < 18) {
+//     window.location.href = "pagina-bloqueo.html";
+//   } else {
+//     alert("Usuario validado");
+//   }
+// }
+
+  
