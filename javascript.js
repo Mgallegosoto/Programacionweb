4e6761c81ef3324e9ced1b93f3cec9d6da68e0f1
Mgallegosoto <mgallegosoto@itba.edu.ar>
2023-06-21 10:44:23 -0300

    push

--- javascript.js
+++ javascript.js
@@ -9,3 +9,21 @@
 //     alert("Usuario validado");
 //   }
 // }
+// Obtener todos los elementos con la clase "photo-zoom"
+
+
+
+// const photoItems = document.querySelectorAll('.photo-zoom');
+
+// // Agregar un evento "mouseover" a cada elemento
+// photoItems.forEach(photo => {
+//   photo.addEventListener('mouseover', () => {
+//     photo.style.transform = 'scale(1.1)'; // Aumentar la escala de la imagen en un 10%
+//   });
+
+//   // Agregar un evento "mouseout" a cada elemento
+//   photo.addEventListener('mouseout', () => {
+//     photo.style.transform = 'scale(1)'; // Restaurar la escala de la imagen a su tamaño original
+//   });
+// });
+
